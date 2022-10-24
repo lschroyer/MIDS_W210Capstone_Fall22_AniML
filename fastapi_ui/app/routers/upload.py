c4e2ce848e08ed0eaf1fdfdd01ac0230ec85f224
Lucas <lucas@brightnightpower.com>
2022-10-24 12:50:08 -0700

    update upload script

--- fastapi_ui/app/routers/upload.py
+++ fastapi_ui/app/routers/upload.py
@@ -33,89 +33,24 @@ def upload(file: UploadFile = File(...)):
         file.file.close()
 
     try:
-        # s3 = boto3.client('s3')
-        # s3.meta.client.upload_file(destination_file_path, 's3://famlive/training_upload/', file.filename)
-        # logging.info("------session attempt------")
-
-        # s3 = boto3.resource('s3')
-        # s3bucket = s3.Bucket('famlive').Object('training_upload')
-        # # s3bucket = s3.Bucket('famlive').Object('training_upload/')
-        # logging.info(s3bucket.name)
-
-        # logging.info("-----bucket post attempt-----")
-        # logging.info(destination_file_path)
-
-
-
-        # client = boto3.client('s3')
-        # # client.upload_file(destination_file_path, s3bucket.name)#, file.filename)
-        # client.upload_file(Filename = destination_file_path, Bucket = "famlive", Key = file.filename)
-        # # client.upload_file(destination_file_path, "^s3://famlive/training_upload/$", file.filename)
-
         s3 = boto3.client("s3")
         bucket_name = "famlive"
         bucket_folder = "training_upload"
-        object_name = file_name #"sample_file.txt"
-        # file_name = os.path.join(pathlib.Path(__file__).parent.resolve(), "sample_file.txt")
+        object_name = file_name 
         local_file_path = os.path.join(pathlib.Path(__file__).parent.resolve(), "../../" + destination_file_path)
 
         response = s3.upload_file(local_file_path, bucket_name, '%s/%s' % (bucket_folder,object_name ))
 
 
-        # response = s3.upload_file(file_name, bucket_name, '%s/%s' % (bucket_folder, object_name))
-        # # pprint(response)
 
-        # logging.debug("-----S3 Debug-----")
-        # s3 = boto3.client("s3")
-        # bucket_name = "famlive"
-        # bucket_folder = "training_upload"
-        # object_name = "sample_file_Lucas.txt"
-        # file_name = os.path.join(pathlib.Path(__file__).parent.resolve(), "sample_file_Lucas.txt")
-
-        # response = s3.upload_file(file_name, bucket_name, '%s/%s' % (bucket_folder,object_name ))
-    
-
-        # s3 = boto3.client("s3")
-        # bucket_name = "famlive"
-        # bucket_folder = "training_upload"
-        # object_name = "sample_file_Lucas_v2.txt"
-        # file_name = os.path.join(pathlib.Path(__file__).parent.resolve(), "sample_file_Lucas_v2.txt")
-
-        # response = s3.upload_file(file_name, bucket_name, '%s/%s' % (bucket_folder,object_name ))
 
 
     except Exception:
         raise HTTPException(status_code=400, detail="There was an error uploading the file to S3")
-        # return {"message": "There was an error uploading the file to S3"}
         
 
     return {"message": f"Successfuly uploaded {file.filename}"}
 
-
-
-# @router.post("/upload")
-# async def upload(files: List[UploadFile] = File(...)):
-#     for file in files:
-#         destination_file_path = "images_uploads/"+file.filename #output file path
-#         async with aiofiles.open(destination_file_path, 'wb') as out_file:
-#             while content := await file.read(1024):  # async read file chunk
-#               await out_file.write(content)  # async write file chunk
-#     return {"Result": "OK", "filenames": [file.filename for file in files]}
-
-# @router.post("/upload")
-# def upload(files: List[UploadFile] = File(...)):
-#     for file in files:
-#         destination_file_path = "images_uploads/"+file.filename #output file path
-#         try:
-#             contents = file.file.read()
-#             with open(destination_file_path, "wb") as f:
-#                 f.write(contents)
-#         except Exception:
-#             return {"message": "There was an error uploading the file"}
-#         finally:
-#             file.file.close()
-#     return {"message": "Successfuly uploaded"}
-#
 
 @router.get("/upload")
 def main(request: Request):
