4ff3a7ab040e0755d24be6d4f771c6cdb8e9fb89
Lucas <lucas@brightnightpower.com>
2022-10-24 12:35:34 -0700

    created post to S3 bucket

--- fastapi_ui/app/routers/upload.py
+++ fastapi_ui/app/routers/upload.py
@@ -1,6 +1,11 @@
 import logging
+import boto3
+from botocore.exceptions import ClientError
+import os
+import pathlib
 
-from fastapi import Request, UploadFile, File, APIRouter
+
+from fastapi import Request, UploadFile, File, APIRouter, HTTPException
 from fastapi.templating import Jinja2Templates
 import aiofiles
 from typing import List
@@ -8,18 +13,81 @@ from typing import List
 router = APIRouter()
 templates = Jinja2Templates(directory="templates/")
 
+logging.basicConfig()
+logging.root.setLevel(logging.NOTSET)
+logging.basicConfig(level=logging.NOTSET)
+logging.info("end of logging configs")
 
 @router.post("/upload")
 def upload(file: UploadFile = File(...)):
     destination_file_path = "images_uploads/"+file.filename #output file path
+    file_name = file.filename
+    logging.debug("Logging Test")
     try:
         contents = file.file.read()
         with open(destination_file_path, "wb") as f:
             f.write(contents)
     except Exception:
-        return {"message": "There was an error uploading the file"}
+        raise HTTPException(status_code=404, detail="There was an error uploading the file")
     finally:
         file.file.close()
+
+    try:
+        # s3 = boto3.client('s3')
+        # s3.meta.client.upload_file(destination_file_path, 's3://famlive/training_upload/', file.filename)
+        # logging.info("------session attempt------")
+
+        # s3 = boto3.resource('s3')
+        # s3bucket = s3.Bucket('famlive').Object('training_upload')
+        # # s3bucket = s3.Bucket('famlive').Object('training_upload/')
+        # logging.info(s3bucket.name)
+
+        # logging.info("-----bucket post attempt-----")
+        # logging.info(destination_file_path)
+
+
+
+        # client = boto3.client('s3')
+        # # client.upload_file(destination_file_path, s3bucket.name)#, file.filename)
+        # client.upload_file(Filename = destination_file_path, Bucket = "famlive", Key = file.filename)
+        # # client.upload_file(destination_file_path, "^s3://famlive/training_upload/$", file.filename)
+
+        s3 = boto3.client("s3")
+        bucket_name = "famlive"
+        bucket_folder = "training_upload"
+        object_name = file_name #"sample_file.txt"
+        # file_name = os.path.join(pathlib.Path(__file__).parent.resolve(), "sample_file.txt")
+        local_file_path = os.path.join(pathlib.Path(__file__).parent.resolve(), "../../" + destination_file_path)
+
+        response = s3.upload_file(local_file_path, bucket_name, '%s/%s' % (bucket_folder,object_name ))
+
+
+        # response = s3.upload_file(file_name, bucket_name, '%s/%s' % (bucket_folder, object_name))
+        # # pprint(response)
+
+        # logging.debug("-----S3 Debug-----")
+        # s3 = boto3.client("s3")
+        # bucket_name = "famlive"
+        # bucket_folder = "training_upload"
+        # object_name = "sample_file_Lucas.txt"
+        # file_name = os.path.join(pathlib.Path(__file__).parent.resolve(), "sample_file_Lucas.txt")
+
+        # response = s3.upload_file(file_name, bucket_name, '%s/%s' % (bucket_folder,object_name ))
+    
+
+        # s3 = boto3.client("s3")
+        # bucket_name = "famlive"
+        # bucket_folder = "training_upload"
+        # object_name = "sample_file_Lucas_v2.txt"
+        # file_name = os.path.join(pathlib.Path(__file__).parent.resolve(), "sample_file_Lucas_v2.txt")
+
+        # response = s3.upload_file(file_name, bucket_name, '%s/%s' % (bucket_folder,object_name ))
+
+
+    except Exception:
+        raise HTTPException(status_code=400, detail="There was an error uploading the file to S3")
+        # return {"message": "There was an error uploading the file to S3"}
+        
 
     return {"message": f"Successfuly uploaded {file.filename}"}
 
