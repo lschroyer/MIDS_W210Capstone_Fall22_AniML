9910eea4488ae01c498547d4a5663057a82015f2
Lucas <lucas@brightnightpower.com>
2022-11-06 16:09:15 -0800

    updated analytics page. training and inference server worked

--- fastapi_ui/app/routers/analytics.py
+++ fastapi_ui/app/routers/analytics.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import json
 
+import pandas as pd
 import logging
 
 # setup loggers
@@ -44,7 +45,20 @@ def parse_yolo_json(yolo_json_path):
     return image_ids, pred_classes, bboxes, confs
 
 
+
+
 test_yolo_image_ids, test_yolo_pred_classes, test_yolo_bboxes, test_yolo_confs = parse_yolo_json("../data/yolo_v5_prediction.json")
+
+test_yolo_pred_classes_new = test_yolo_pred_classes.copy()
+
+def filter_low_conf_images(conf_level):
+    for index, image_id in enumerate(test_yolo_image_ids):
+        if test_yolo_confs[index] < conf_level:
+            test_yolo_pred_classes_new[index] = 0
+        else:
+            test_yolo_pred_classes_new[index] = 1
+    return test_yolo_pred_classes_new
+
 
 @router.get("/analytics", response_class=HTMLResponse)
 def form_get(request: Request):
@@ -65,7 +79,24 @@ def form_post2(request: Request, number: int = Form(...)):
     result = number + 100
     return templates.TemplateResponse('analytics.html', context={'request': request, 'result': result, 'yournum': number})
 
-@router.get("/form3", response_class=HTMLResponse)
-def form_post3(request: Request):
-    result = test_yolo_image_ids
-    return templates.TemplateResponse('analytics.html', context={'request': request, 'result': result, 'yournum': ''})
+@router.post("/form3", response_class=HTMLResponse)
+def form_post3(request: Request, conf_lev: float = Form(...)):
+    pred_classes_new = filter_low_conf_images(conf_lev)
+    model_predictions = {"image_ids": test_yolo_image_ids, 
+            "confidence_scores": test_yolo_confs, 
+            "predicted_classes_old": test_yolo_pred_classes, 
+            "predicted_classes_new": pred_classes_new}
+
+    model_predictions_df = pd.DataFrame.from_dict(model_predictions)
+
+
+    #render dataframe as html
+    html = model_predictions_df.to_html()
+
+    #write html to file
+    text_file = open("templates/classification_table.html", "w")
+    text_file.write(html)
+    text_file.close()
+
+    return templates.TemplateResponse('analytics.html', context={'request': request, 'model_predictions': model_predictions_df})
+
