eb577f7924971511138fa321879100d78c9d8fbe
Ivan Wong - ivanwong-berkeley <ivanwong@berkeley.edu>
2022-12-06 20:06:37 -0800

    Add _farmlive2_anymlws_data > fastapi_animl_ws folder.

---
+++ backend/_farmlive2_anymlws_data/fastapi_animl_ws/app/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from .library.helpers import *
+from app.routers import twoforms, unsplash, accordion
+
+
+app = FastAPI()
+
+
+templates = Jinja2Templates(directory="templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.include_router(unsplash.router)
+app.include_router(twoforms.router)
+app.include_router(accordion.router)
+
+
+# @app.get("/", response_class=HTMLResponse)
+# async def home(request: Request):
+#     data = openfile("home.md")
+#     return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    data = openfile("index.html")
+    return templates.TemplateResponse("page.html", context={"request": request, "data": data})
+
+
+@app.get("/page/detection", response_class=HTMLResponse)
+async def detection(request: Request):
+    data = openfile("index.html")
+    return templates.TemplateResponse("page.html", context={"request": request, "data": data})
+
+@app.get("/page/training", response_class=HTMLResponse)
+async def detection(request: Request):
+    data = openfile("training.html")
+    return templates.TemplateResponse("page.html", context={"request": request, "data": data})
+
+@app.get("/page/{page_name}", response_class=HTMLResponse)
+async def show_page(request: Request, page_name: str):
+    data = openfile(page_name+".md")
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
